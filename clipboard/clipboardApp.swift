ac484ff11b1a3077a9f70cce0c8659b1f2accb2a
Jopaul Joy <jopauljoy999@gmail.com>
2024-12-13 14:11:23 +0530

    Initial Commit

---
+++ clipboard/clipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardApp.swift
+//  clipboard
+//
+//  Created by Jopaul Joy on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct clipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
